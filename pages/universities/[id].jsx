b65418d601513db31d367439ab21f10dd6fd4b14
emporteme <magzhankarataev02@gmail.com>
2023-06-06 12:36:41 +0600

    university details

--- pages/universities/[id].jsx
+++ pages/universities/[id].jsx
@@ -2,66 +2,58 @@
 import MainLayout from "@/components/MainLayout";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import unikData from "../../data/unik.json";
+// import unikData from "../../data/unik.json";
 import styles from "../../styles/unikDetails.module.scss"
+import ApiClient from '../../api/ApiClient';
 
 export default function UniversityDetails() {
     const router = useRouter();
     const [university, setUniversity] = useState({
-        pk: 0,
-        model: "",
-        fields: {
-            university_name: "",
-            short_name: "",
-            location: "",
-            latitude: 0,
-            longitude: 0,
-            description: "",
-            website: "",
-            logo: "",
-            established_year: 0,
-            study_type: [],
-            study_language: [],
-            study_year: [],
-            student_count: 0,
-            faculty_count: 0,
-            faculty: [],
-            edu_programm: [],
-            email: "",
-            phone_number: [],
-            min_unt: 0,
-            min_ielts: 0,
-            tuition: 0,
-            admission_criteria: [],
-            dormitory: "",
-            student_life: "",
-            alumni: [],
-            athletics: "",
-            financial_aid: "",
-            required_tests: [],
-            application_fee: "",
-            application_deadline: "",
-            acceptance_rate: "",
-            internship_opportunities: "",
-            study_abroad_programs: [],
-            clubs_and_organizations: [],
-            social_media: [],
-            accreditation: "",
-            source: "",
-        },
+        id: 0,
+        specialities: [],
+        language: [],
+        university_name: "",
+        short_name: "",
+        university_code: "",
+        location: "",
+        email: "",
+        phone_number: "",
+        number_of_specialties: 0,
+        student_count: 0,
+        website: "",
+        established_year: 0,
+        tuition_price: 0,
+        description: "",
+        logo: "",
+        main_img: "",
+        min_unt: 0,
+        min_ielts: 0,
+        about_university: "",
+        accreditation: "",
+        latitude: "",
+        longitude: "",
+        carousel_1: "",
+        carousel_2: "",
+        carousel_3: "",
+        facebook: "",
+        instagram: "",
+        youtube: "",
     });
 
     useEffect(() => {
         const { id } = router.query;
         if (typeof id === "string") {
-            const universityData = unikData.find(
-                (item) => item.pk === parseInt(id)
-            );
-            if (universityData) {
-                setUniversity(universityData);
-            }
+            ApiClient.getUnik().then(unikData => {
+                const universityData = unikData.find(
+                    (item) => item.id === parseInt(id)
+                );
+                if (universityData) {
+                    setUniversity(universityData);
+                }
+            });
         }
     }, [router.query]);
+
 
     if (!university) {
         return <div>Loading...</div>;
@@ -69,13 +61,24 @@ export default function UniversityDetails() {
 
     return (
         <MainLayout
-            title={`Diploma | ${university?.fields?.university_name}`}
+            title={`Diploma | ${university?.university_name}`}
             spacing="2vw 5vw"
         >
             <div className={styles.university}>
-                <h1>{university.fields.university_name}</h1>
-                <h2>{university.fields.short_name}</h2>
-                <img src={university.fields.logo} alt="University Logo" />
+                <h1>{university.university_name}</h1>
+                <img src={university.logo} className={styles.logo} alt="University Logo" />
+                <h2>{university.short_name}</h2>
+                <div className={styles.gallery}>
+                    <div className={styles.image}>
+                        <img src={university.carousel_1} alt={university.carousel_1} />
+                    </div>
+                    <div className={styles.image}>
+                        <img src={university.carousel_2} alt={university.carousel_2} />
+                    </div>
+                    <div className={styles.image}>
+                        <img src={university.carousel_3} alt={university.carousel_3} />
+                    </div>
+                </div>
                 <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
                     <div className={styles.navbar} style={{ width: '25%' }}>
                         <ul>
@@ -111,55 +114,60 @@ export default function UniversityDetails() {
                         </ul>
                     </div>
                     <div className={styles.universityDetails} style={{ width: '75%' }}>
-                        <p id="Location"><strong>Location:</strong> {university.fields.location}</p>
-                        <p id="Description"><strong>Description:</strong> {university.fields.description}</p>
-                        <p id="Website"><strong>Website:</strong> <a href={university.fields.website}>{university.fields.website}</a></p>
-                        <p id="Established_Year"><strong>Established Year:</strong> {university.fields.established_year}</p>
-                        <p id="Type"><strong>Study Type:</strong> {university.fields.study_type.join(", ")}</p>
-                        <p id="Language"><strong>Study Language:</strong> {university.fields.study_language.join(", ")}</p>
-                        <p id="Year"><strong>Study Year:</strong> {university.fields.study_year.join(", ")}</p>
-                        <p id="Student"><strong>Student Count:</strong> {university.fields.student_count}</p>
-                        <p id="Faculty"><strong>Faculty Count:</strong> {university.fields.faculty_count}</p>
-                        <p id="Faculties"><strong>Faculties:</strong> {university.fields.faculty.join(", ")}</p>
-                        <p id="Programs"><strong>Educational Programs:</strong></p>
+                        <p id="Location"><strong>Location:</strong> {university.location}</p>
+                        <p id="Description"><strong>Description:</strong> {university.description}</p>
+                        <p id="Website"><strong>Website:</strong> <a href={university.website}>{university.website}</a></p>
+                        <p id="Established_Year"><strong>Established Year:</strong> {university.established_year}</p>
+                        <p id="Language"><strong>Study Language:</strong> {university.language.map(lang => lang.name).join(", ")}</p>
+                        {/* <p id="Year"><strong>Study Year:</strong> {university.study_year.join(", ")}</p> */}
+                        <p id="Student"><strong>Student Count:</strong> {university.student_count}</p>
+                        <p id="Faculty"><strong>Faculty Count:</strong> {university.faculty_count}</p>
+                        {/* <p id="Faculties"><strong>Faculties:</strong> {university.faculty.join(", ")}</p> */}
+                        {/* <p id="Programs"><strong>Educational Programs:</strong></p>
                         <ul>
-                            {university.fields.edu_programm.map((item, index) => (
+                            {university.edu_programm.map((item, index) => (
                                 <li key={index}>{item}</li>
                             ))}
-                        </ul>
-                        <p id="Email"><strong>Email:</strong> {university.fields.email}</p>
-                        <p id="Phone"><strong>Phone Number:</strong> {university.fields.phone_number.join(", ")}</p>
-                        <p id="UNT"><strong>Minimum UNT Score:</strong> {university.fields.min_unt}</p>
-                        <p id="IELTS"><strong>Minimum IELTS Score:</strong> {university.fields.min_ielts}</p>
-                        <p id="Tuition"><strong>Tuition Fee:</strong> {university.fields.tuition}</p>
-                        <p id="Criteria"><strong>Admission Criteria:</strong> {university.fields.admission_criteria.join(", ")}</p>
-                        <p id="Dormitory"><strong>Dormitory:</strong> {university.fields.dormitory}</p>
-                        <p id="Life"><strong>Student Life:</strong> {university.fields.student_life}</p>
-                        <p id="Alumni"><strong>Alumni:</strong></p>
+                        </ul> */}
+                        <p id="Email"><strong>Email:</strong> {university.email}</p>
+                        {/* <p id="Phone"><strong>Phone Number:</strong> {university.phone_number.join(", ")}</p> */}
+                        <p id="UNT"><strong>Minimum UNT Score:</strong> {university.min_unt}</p>
+                        <p id="IELTS"><strong>Minimum IELTS Score:</strong> {university.min_ielts}</p>
+                        <p id="Tuition"><strong>Tuition Fee:</strong> {university.tuition}</p>
+                        {/* <p id="Criteria"><strong>Admission Criteria:</strong> {university.admission_criteria.join(", ")}</p> */}
+                        <p id="Dormitory"><strong>Dormitory:</strong> {university.dormitory}</p>
+                        <p id="Life"><strong>Student Life:</strong> {university.student_life}</p>
+                        {/* <p id="Alumni"><strong>Alumni:</strong></p>
                         <ul>
-                            {university.fields.alumni.map((item, index) => (
+                            {university.alumni.map((item, index) => (
                                 <li key={index}>{item}</li>
                             ))}
-                        </ul>
-                        <p id="Athletics"><strong>Athletics:</strong> {university.fields.athletics}</p>
-                        <p id="Financial"><strong>Financial Aid:</strong> {university.fields.financial_aid}</p>
-                        <p id="Tests"><strong>Required Tests:</strong> {university.fields.required_tests.join(", ")}</p>
-                        <p id="Fee"><strong>Application Fee:</strong> {university.fields.application_fee}</p>
-                        <p id="Deadline"><strong>Application Deadline:</strong> {university.fields.application_deadline}</p>
-                        <p id="Acceptance"><strong>Acceptance Rate:</strong> {university.fields.acceptance_rate}%</p>
-                        <p id="Internship"><strong>Internship Opportunities:</strong> {university.fields.internship_opportunities}</p>
-                        <p id="Abroad"><strong>Study Abroad Programs:</strong></p>
+                        </ul> */}
+                        <p id="Athletics"><strong>Athletics:</strong> {university.athletics}</p>
+                        <p id="Financial"><strong>Financial Aid:</strong> {university.financial_aid}</p>
+                        {/* <p id="Tests"><strong>Required Tests:</strong> {university.required_tests.join(", ")}</p> */}
+                        <p id="Fee"><strong>Application Fee:</strong> {university.application_fee}</p>
+                        <p id="Deadline"><strong>Application Deadline:</strong> {university.application_deadline}</p>
+                        <p id="Acceptance"><strong>Acceptance Rate:</strong> {university.acceptance_rate}%</p>
+                        <p id="Internship"><strong>Internship Opportunities:</strong> {university.internship_opportunities}</p>
+                        <p id="Specialities"><strong>Specialities:</strong></p>
                         <ul>
-                            {university.fields.study_abroad_programs.map((item, index) => (
-                                <li key={index}>{item}</li>
+                            {university.specialities.map((item, index) => (
+                                <li key={index}>{item.speciality_name}</li>
                             ))}
                         </ul>
-                        <p id="Clubs"><strong>Clubs and Organizations:</strong></p>
+                        {/* <p id="Abroad"><strong>Study Abroad Programs:</strong></p>
                         <ul>
-                            {university.fields.clubs_and_organizations.map((item, index) => (
+                            {university.study_abroad_programs.map((item, index) => (
                                 <li key={index}>{item}</li>
                             ))}
-                        </ul>
+                        </ul> */}
+                        {/* <p id="Clubs"><strong>Clubs and Organizations:</strong></p>
+                        <ul>
+                            {university.clubs_and_organizations.map((item, index) => (
+                                <li key={index}>{item}</li>
+                            ))}
+                        </ul> */}
                     </div>
                 </div>
             </div>
